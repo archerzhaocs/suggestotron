49289d0a02d85502997c507a2c021e6ecc432e00
Archer Zhao <changlechangsheng@gmail.com>
2017-04-15 17:00:46 +0800

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
       post 'downvote'
     end
   end
+
+  get 'about' => 'topics#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "topics#index"
 end
